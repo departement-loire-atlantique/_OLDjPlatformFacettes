3523cd7e31683c21ac850921c94c2f091fdbb377
formont <mickael.formont@trsb.net>
2019-05-06 12:02:56 +0200

    Ajout de dépendance vers socle

--- WEB-INF/classes/fr/cg44/plugin/facettes/queryfilter/CityQueryFilter.java
+++ WEB-INF/classes/fr/cg44/plugin/facettes/queryfilter/CityQueryFilter.java
@@ -23,41 +23,49 @@ import generated.City;
  * Filtre pour la facette commune.
  */
 public class CityQueryFilter extends QueryFilter {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(CityQueryFilter.class);
 
 	public boolean init(Plugin plugin) {	 
 		LOGGER.debug("CityQueryFilter is enabled");
 		return true;
 	}
-	
-	
+
+
 	public QueryHandler filterQueryHandler(QueryHandler qh, Map context) {
-				
+
 		HttpServletRequest request = getChannel().getCurrentServletRequest();	
 		if(Util.isEmpty(request)) {
 			return qh;
 		}
-	    Data cityData = HttpUtil.getDataParameter(request, "commune");
-		
-	    if(Util.notEmpty(cityData) && cityData instanceof City) {
-	    	
-	    	qh.setMode("advanced");	    	
-	    	City city = (City) cityData;
-	    	String cityCode =  Integer.toString(city.getCityCode());
-	    	
-	    	String prevSearchText = "";
-	    	if(Util.notEmpty(qh.getText())) {
-	    		prevSearchText = qh.getText() + " AND ";
-	    	}
-	    	
-	    	qh.setText(prevSearchText + "(" + PublicationFacetedSearchCityEnginePolicyFilter.INDEX_FIELD_CITY + ":\"" + cityCode +"\")");	    	
+		Data cityData = HttpUtil.getDataParameter(request, "commune");
+		addCitySearch(qh, cityData);
 
-	    }
-		    
 		return qh;
 	}
-	
-	
-	
+
+
+	/**
+	 * Ajoute le filtre sur la commune
+	 * Attention la query passe en syntaxe de recherche avancée
+	 * @param qh
+	 * @param cityData
+	 */
+	public static void addCitySearch(QueryHandler qh, Data cityData) {
+		if(Util.notEmpty(cityData) && cityData instanceof City) {
+			qh.setMode("advanced");	    	
+			City city = (City) cityData;
+			String cityCode =  Integer.toString(city.getCityCode());
+
+			String prevSearchText = "";
+			if(Util.notEmpty(qh.getText())) {
+				prevSearchText = qh.getText() + " AND ";
+			}	
+			
+			qh.setText(prevSearchText + "(" + PublicationFacetedSearchCityEnginePolicyFilter.INDEX_FIELD_CITY + ":\"" + cityCode +"\")");	    	
+		}	
+	}
+
+
+
 }
